cb91f11d54721d9569d294ea368992ad9c2d8bb6
Nguyen Quang Huy <trannguyenhan01092000@gmail.com>
2020-04-27 15:46:19 +0700

    m

---
+++ src/btl/BTL.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package btl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author admin
+ */
+public class BTL {
+    public static void main(String[] args) throws IOException {
+        ReadFile rfVNINDEX = new ReadFile();
+        ReadFile rfUPCOMINDEX = new ReadFile();
+        ReadFile rfHNXINDEX = new ReadFile();
+        
+        rfVNINDEX.readFileExcel("C:/Users/admin/Downloads/VN-INDEX-FILE.xlsx");
+        rfUPCOMINDEX.readFileExcel("C:/Users/admin/Downloads/UPCOM-INDEX(1).xlsx");
+        rfHNXINDEX.readFileExcel("C:/Users/admin/Downloads/HNX-INDEX(1).xlsx");
+
+        // File dau ra 
+        PrintWriter printWriter = new PrintWriter("ketqua.txt");
+        
+        // Nhom 5-0
+        for(int i=0; i<rfVNINDEX.totalRow; i++)
+        {
+            Nhom5_0 nhom5_0 = new Nhom5_0(rfVNINDEX.list[i],rfUPCOMINDEX.list[i],rfHNXINDEX.list[i]);
+            nhom5_0.printResult(printWriter);
+        }
+        
+        // Nhom 5-1
+        for(int i=0; i<rfUPCOMINDEX.totalRow; i++)
+        {
+            Nhom5_1 nhom5_1 = new Nhom5_1(rfVNINDEX.list[i], rfUPCOMINDEX.list[i], rfHNXINDEX.list[i]);
+            nhom5_1.printResult(printWriter);
+        }
+        
+        // Nhom 5-2
+        for(int i=0; i<rfUPCOMINDEX.totalRow; i++)
+        {
+            Nhom5_2 nhom5_2 = new Nhom5_2(rfVNINDEX.list[i], rfUPCOMINDEX.list[i], rfHNXINDEX.list[i]);
+            nhom5_2.printResult(printWriter);
+        }
+        
+        // Dong file sau khi ket thuc in tat ca cac lenh
+        printWriter.close();
+        
+    }
+}
---
+++ src/btl/ReadFile.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package btl;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.formula.functions.Column;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.*;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author admin
+ */
+public class ReadFile {
+
+    private final int MAX_ROW = 200;
+    private XSSFWorkbook wb;
+    public DSChiSo[] list;
+    public int totalRow;
+    
+    public ReadFile()
+    {
+        list = new DSChiSo[MAX_ROW];
+        totalRow = 0;
+    }
+    
+    public String getCellValue(XSSFCell cell)
+    {
+        String stringTemp;
+        switch(cell.getCellType())
+        {
+            case STRING:
+                stringTemp = cell.getStringCellValue();
+                break;
+            case NUMERIC: 
+                double doubleTemp = cell.getNumericCellValue();
+                stringTemp = String.valueOf(doubleTemp);
+            default:
+                stringTemp = "NULL";
+        }
+        
+        return stringTemp;
+    }
+    
+    public void readFileExcel(String path) throws IOException
+    {
+        XSSFWorkbook wb = new XSSFWorkbook(path);
+        XSSFSheet sh = wb.getSheetAt(0); // Lay sheet dau tien
+        
+        XSSFRow row = sh.getRow(1);
+        XSSFCell cell;
+        
+        while(row != null)
+        {
+            cell = row.getCell(0);
+            if(cell == null) break;
+            String value0 = new SimpleDateFormat("dd/MM/yyyy").format(cell.getDateCellValue());
+            
+            cell = row.getCell(1);
+            String value1 = getCellValue(cell);
+            
+            cell = row.getCell(2);
+            String value2 = getCellValue(cell);
+            
+            cell = row.getCell(3);
+            String value3 = getCellValue(cell);
+            
+            cell = row.getCell(4);
+            String value4 = getCellValue(cell);
+            
+            cell = row.getCell(5);
+            String value5 = getCellValue(cell);
+            
+            cell = row.getCell(6);
+            String value6 = getCellValue(cell);
+            
+            cell = row.getCell(7);
+            String value7 = getCellValue(cell);
+            
+            cell = row.getCell(8);
+            String value8 = getCellValue(cell);
+            
+            cell = row.getCell(9);
+            String value9 = getCellValue(cell);
+            
+            list[totalRow] = new DSChiSo(value0, value1, value2, value3, value4, value5, value6, value7, value8, value9);
+            totalRow++;
+            
+            row = sh.getRow(totalRow+1);
+            
+        }
+        
+        return;
+    }   
+}
